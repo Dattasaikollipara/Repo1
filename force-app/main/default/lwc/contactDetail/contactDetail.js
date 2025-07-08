c4bd56ab65959f698fcefff999ca7207b30ad88b
kollipara94 <hemalatakollipara@gmail.com>
2025-07-08 18:40:28 +0530

    Updating deleting deploy components on lwc

--- force-app/main/default/lwc/contactDetail/contactDetail.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ContactDetail extends LightningElement {}
--- force-app/main/default/lwc/contactTile/contactTile.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ContactTile extends LightningElement {
-
-    /*** Create a method to Calculate the Sum of two numbers */
-    calculateSum(num1, num2) {
-        return num1 + num2;
-    }
-}
